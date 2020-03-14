7545262590a1bfe2fd3ff275a0cb5b82660403b1
confusement <sattri99@gmail.com>
2020-03-14 21:45:14 +0530

    changed tables to Cards

--- public/javascripts/userDash.js
+++ public/javascripts/userDash.js
@@ -51,31 +51,40 @@ function fillContent()
 				service_id:"FOO00001",
 				name:"Food1",
 				cuisine:"BUDBDUBDU",
-				res_name:"Meisfn",
-				locality:"neslo",
-				city:"groxmi",
-				delivery:"Y"
+				res_name:"Res name",
+				locality:"locality",
+				city:"city",
+				delivery:"Y",
+				price:550,
+				discount:0,
+				rating:3.9
 			},
 			{
 				service_id:"FOO00002",
 				name:"Food2",
 				cuisine:"NSHDSU",
-				res_name:"Mef",
-				locality:"nessaga",
-				city:"groxag",
-				delivery:"N"
+				res_name:"Res name",
+				locality:"locality",
+				city:"city",
+				delivery:"N",
+				price:150,
+				discount:20,
+				rating:4.1
 			},
 			{
 				service_id:"FOO00003",
 				name:"Food3",
-				cuisine:"NSHasDSU",
-				res_name:"Mef",
-				locality:"nessaga",
-				city:"groxag",
-				delivery:"N"
+				cuisine:"Description of Item",
+				res_name:"Res name",
+				locality:"locality",
+				city:"city",
+				delivery:"N",
+				price:200,
+				discount:15,
+				rating:4.5
 			}
 		];
-		var attribs=['name','cuisine','res_name','locality','city','delivery'];
+		var attribs=['name','cuisine','res_name','locality','city','delivery','price','discount'];
 		//remove existing Rows tr Elemens
 		tableElement=document.getElementById("table2");
 		for (let i=tableElement.childNodes.length-1;i>=0;i--) {
@@ -84,27 +93,53 @@ function fillContent()
 		 //Add Rows
 		for(let i=0;i<queryResult.length;i++)
 		{
-			let trElement=document.createElement("tr");
-			let numElement=document.createElement("th");
-			numElement.scope="row";
-			numElement.innerText=i+1;
-			trElement.appendChild(numElement);
-			attribs.forEach(element => {
-				let newElement=document.createElement("td");
-				newElement.innerText=queryResult[i][element];
-				trElement.appendChild(newElement);
-			});
-			let bookTd=document.createElement("td");
+			// console.log("adding "+i);
+			let outerCard=document.createElement("div");
+			outerCard.className="rowElement card shadow p-4 mb-4 bg-white";
+			
+			let headerCard=document.createElement("div");
+			headerCard.className="card-header text-center bg-white align-middle";
+			headerCard.innerText="Restaurant: "+queryResult[i].res_name+","+queryResult[i].locality+","+queryResult[i].city;
+
+			let bodyCard=document.createElement("div");
+			bodyCard.className="card-body text-center d-flex flex-row justify-content-between align-middle";
+			let nameCard=document.createElement("div");
+			nameCard.className="cardText card-text";
+			nameCard.innerHTML=queryResult[i].name+"<br>Delivery Available: "+((queryResult[i].delivery=="Y")?("Yes"):("No"));
+			let middleCard=document.createElement("div");
+			middleCard.className="cardText card-text";
+			middleCard.innerHTML="Discount: "+queryResult[i].discount+"%<br> Description: "+ queryResult[i].cuisine;
 			let bookButton=document.createElement("Button");
 			bookButton.className="btn btn-outline-primary";
-			bookButton.textContent="Request";
+			bookButton.textContent="Request ("+queryResult[i].price+" Rs)";
 			bookButton.addEventListener("click", function(){
 				alert("booked service: "+queryResult[i].service_id);
 			  });
-			bookTd.appendChild(bookButton);
-			trElement.appendChild(bookTd);
-			tableElement.appendChild(trElement);
+
+			bodyCard.appendChild(nameCard);
+			bodyCard.appendChild(middleCard);
+			bodyCard.appendChild(bookButton);
+
+			footerCard=document.createElement("div");
+			footerCard.className="card-footer text-center d-flex flex-row justify-content-between align-middle bg-white align-middle";
+			ratingCard=document.createElement("div");
+			ratingCard.className="cardText card-text";
+			ratingCard.innerText="Rating: "+queryResult[i].rating+"/5";
+			let reviewButton=document.createElement("Button");
+			reviewButton.className="btn btn-outline-primary";
+			reviewButton.textContent="View Reviews";
+			reviewButton.addEventListener("click", function(){
+				alert("Open popUp Reviews for "+queryResult[i].service_id);
+			  });
+			footerCard.appendChild(ratingCard);
+			footerCard.appendChild(reviewButton);
+
+			outerCard.appendChild(headerCard);
+			outerCard.appendChild(bodyCard);
+			outerCard.appendChild(footerCard);
+			tableElement.appendChild(outerCard);
 		}
+		
 	}
 	//Add other domains and Booking status
 	else{

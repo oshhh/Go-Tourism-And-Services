2ac6b604c21727dfbc5f3d807775dfc473c32d55
osheensachdev <osheen18059@iiitd.ac.in>
2020-04-25 00:02:27 +0530

    .

--- server.js
+++ server.js
@@ -131,15 +131,15 @@ function handleDisconnect() {
         console.log('error when connecting to db:', err);
         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
       }                                     // to avoid a hot loop, and to allow our node script to
-});                                     // process asynchronous requests in the meantime.
+    });                                     // process asynchronous requests in the meantime.
                                             // If you're also serving http, display a 503 error.
-con.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-    }
+    con.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
     });
 }
 
